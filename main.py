f093991b428ecd727a85c9cf7542f395c6c8b505
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2023-09-14 14:07:14 -0300

    Criando verificação de usuário no banco de dados

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ resposta = int(input("Seja bem vindo(a) escolha uma das opções abaixo para con
 if resposta==1:
     usuario = input("Digite o nome do Usuário ").title()
     senha = input("Digite a Sua Senha ")
+    for index,lista in bancoDados.apolices.items():
+        if usuario == lista[4] and senha == lista[5]:
+            print("Usuário encontrado!")
+        else:
+            print("Usuário não cadastrado")
