cf8ea3bbccef13fa7229e1999acdb6ca4a058696
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2023-09-14 12:03:53 -0300

    Criando formulário de entrada do usuário

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 resposta = int(input("Seja bem vindo(a) escolha uma das opções abaixo para continuar 1-Entrar 2-Sou ADM 3-Cancelar "))
 
+if resposta==1:
+    usuario = input("Digite o nome do Usuário ").title()
+    senha = input("Digite a Sua Senha ")
