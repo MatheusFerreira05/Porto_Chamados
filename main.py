edc8cfea731a84d2206ffdf3b813f43579c745b5
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2023-09-15 09:41:20 -0300

    Criando formulário de interação inicial adm

--- main.py
+++ main.py
@@ -12,18 +12,23 @@ if resposta==1:
             print(f"Escolha uma das opções abaixo para continuar: ")
             print("Digite 1 para ver suas informações")
             print("Digite 2 para Falar com o Atendente")
+        
+            opcao = int(input("Entre com a opção desejada: "))
+            if opcao == 1:
+                    for index,lista in bancoDados.apolices.items():
+                        if usuario == lista[4]:
+                            print(lista[0])
+                            print(lista[1])
+                            print(lista[2])
+                            print(lista[3])
+                            print(lista[4])
+                            print(lista[5])
+                    break
+            elif opcao == 2:
+                print("Falando com o atendente")
         else:
             print("Usuário não cadastrado")
-        opcao = int(input("Entre com a opção desejada: "))
-        if opcao == 1:
-                for index,lista in bancoDados.apolices.items():
-                    if usuario == lista[4]:
-                        print(lista[0])
-                        print(lista[1])
-                        print(lista[2])
-                        print(lista[3])
-                        print(lista[4])
-                        print(lista[5])
-                break
-        elif opcao == 2:
-            print("Vamos falar com o atendente")
+
+if resposta == 2:
+    usuario = input("Digite o nome do Usuário ").title()
+    senha = input("Digite a Sua Senha ")
