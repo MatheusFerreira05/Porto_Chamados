8ad77c880c14a34f792dca58b803e2591f07248d
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2023-09-15 11:57:47 -0300

    Criando opções do menu do adm

--- main.py
+++ main.py
@@ -42,3 +42,28 @@ if resposta == 2:
             print("Digite 4 para procurar um usuário específico")
             print("Digite 5 para Sair")
             opcao = int(input("Entre com a opção desejada: "))
+
+            if opcao == 1:
+                for index,usuarios in bancoDados.apolices.items():
+                    print (usuarios)
+            elif opcao == 2:
+                id = list(bancoDados.apolices.keys())
+                bancoDados.apolices[id[len(id)-1]+1] = [input("Entre com a nova apólice: ").title(),
+                input("Entre com o nome completo: "),
+                input("Entre com a data de nascimento incluindo a barra: "),
+                input("Entre com o cpf incluindo pontos e traço: "),
+                input("Entre com o nome do usuário (Primeira letra Maiuscula): "),
+                input("Entre com a senha (Primeira letra Maiuscula): ")]
+                for index,lista in bancoDados.apolices.items():
+                    print(index,lista)
+            elif opcao == 3:
+                busca = input("Entre com o Nome do usuário: ").title()
+                for index, lista in bancoDados.apolices.items():
+                    if busca == lista[0]:
+                        del bancoDados.apolices[busca]
+                        print(index,lista)
+            elif opcao == 4:
+                apoliceBuscada = input("Digite o numero da apólice procurada ")
+                for index,lista in bancoDados.apolices.items():
+                    if apoliceBuscada == lista[0]:
+                        print(index,lista)
