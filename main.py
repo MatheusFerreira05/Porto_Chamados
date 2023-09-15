601f88911dfc8a4aa6346ec06250f788d2263aec
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2023-09-15 09:22:08 -0300

    Criando opções de menu do usuário

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import bancoDados
+import socket
 
 resposta = int(input("Seja bem vindo(a) escolha uma das opções abaixo para continuar 1-Entrar 2-Sou ADM 3-Cancelar "))
 
@@ -13,3 +14,16 @@ if resposta==1:
             print("Digite 2 para Falar com o Atendente")
         else:
             print("Usuário não cadastrado")
+        opcao = int(input("Entre com a opção desejada: "))
+        if opcao == 1:
+                for index,lista in bancoDados.apolices.items():
+                    if usuario == lista[4]:
+                        print(lista[0])
+                        print(lista[1])
+                        print(lista[2])
+                        print(lista[3])
+                        print(lista[4])
+                        print(lista[5])
+                break
+        elif opcao == 2:
+            print("Vamos falar com o atendente")
