a9f296c1a466de0c5c8afe91a7dfcc201a0f1880
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2023-09-19 16:04:00 -0300

    Ajustando Banco de Dados

--- bancoDados.py
+++ bancoDados.py
@@ -1,3 +1,3 @@
-apolices = {1:["12-34-567.890","Matheus da Silva Ferreira","05/04/1975","159.456.581-03","Matheusinho123","@Math789"]}
+usuario = {1:["Adm","Admim123"]}
 
-adm = {1:["Adm","Admim123"]}
+apolices = {"1":["Placa","12-34-567.890","Matheus da Silva Ferreira","05/04/1975","159.456.581-03"]}
