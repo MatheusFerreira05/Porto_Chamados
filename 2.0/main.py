d79131a5318d5cb55ac8f715f8f6bc6837e0c5cc
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2023-09-19 17:10:33 -0300

    Subindo verificação de login

--- 2.0/main.py
+++ 2.0/main.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/login", methods=['POST'])
+def login():
+    usuario = request.form.get('nome')
+    senha = request.form.get('senha')
+    for index,adm in bancoDados.usuario.items():
+        if usuario == adm[0] and senha == adm[1]:
+            return "logou"
+        else:
+            print("Erro")
+            return redirect('/')
+
 if __name__ == "__main__":
     app.run(debug=True)
