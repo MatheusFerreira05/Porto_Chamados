75c2ba72eb2ee7e9cb3ece095e44540c180961bc
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2023-09-20 08:35:44 -0300

    criando rota do formulário de preeenchimento de dados

--- 2.0/main.py
+++ 2.0/main.py
@@ -12,10 +12,14 @@ def login():
     senha = request.form.get('senha')
     for index,adm in bancoDados.usuario.items():
         if usuario == adm[0] and senha == adm[1]:
-            return "logou"
+            return redirect('/form')
         else:
             print("Erro")
             return redirect('/')
+
+@app.route("/form")
+def form_page():
+    return render_template('form.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
